8201ae52c075e5aa28cc3f1a6f315e299e74c28d
victorleon0 <victor.leon.huerta@gmail.com>
2022-06-24 17:03:59 +0100

    START

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { VideogamesComponent } from './pages/videogames/videogames.component'
+import { GestionComponent } from './pages/gestion/gestion.component';
+
+const routes: Routes = [
+  {
+    path: "", pathMatch: "full", component: HomeComponent
+  },
+  {
+    path: "videogames", component: VideogamesComponent
+  },
+  {
+    path: "gestion", component: GestionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/models/videogames.interfaces.ts
+++ front/src/app/models/videogames.interfaces.ts
@@ -6,5 +6,4 @@ export interface VideogamesInterface {
     platform: string[];
     year: number;
     genre: string;
-    
 }
