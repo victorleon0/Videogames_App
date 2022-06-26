71d076680b9f1fb95e9909e6028353d0a4fe91cf
glorvega <gloria.vega@bootcamp-upgrade.com>
2022-06-26 12:57:34 +0200

    stashing changes

--- front/src/app/pages/videogames/videogames.component.ts
+++ front/src/app/pages/videogames/videogames.component.ts
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import { Component, Input, OnInit } from '@angular/core';
-import { VideogamesInterface } from './../../models/videogames.interfaces';
-
-=======
 import { VideogamesInterface } from './../../models/videogames.interfaces';
 import { Component, OnInit } from '@angular/core';
 import { VideogamesService } from 'src/app/services/videogames.service';
 import { Router } from '@angular/router';
->>>>>>> form-edit
 
 @Component({
   selector: 'app-videogames',
@@ -15,17 +9,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./videogames.component.scss']
 })
 export class VideogamesComponent implements OnInit {
-<<<<<<< HEAD
-
-  @Input() public videogamesList!: VideogamesInterface[];
-
-
-
-  constructor() { }
-=======
   public videogamesList: VideogamesInterface[] = [];
   constructor(private videogamesService: VideogamesService, private router: Router) { }
->>>>>>> form-edit
 
   ngOnInit(): void {
     this.videogamesService.getVideogames().subscribe((data:any) =>{
@@ -33,9 +18,9 @@ export class VideogamesComponent implements OnInit {
     })
   }
 
-  public selectedToEdit = (videogame:VideogamesInterface) => {
-    this.videogamesService.sendItemToEdit(videogame);
-    this.router.navigate(['gestion']);
+  public selectedToEdit = (videogames:VideogamesInterface) => {
+    this.videogamesService.sendItemToEdit(videogames);
+    this.router.navigate(['/gestion']);
   }
 
 }
