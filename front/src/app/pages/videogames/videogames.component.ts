f4d880217c1ecb22f8ff173adec534658cd8d38b
glorvega <gloria.vega@bootcamp-upgrade.com>
2022-06-27 20:38:43 +0200

    added sorting buttons

--- front/src/app/pages/videogames/videogames.component.ts
+++ front/src/app/pages/videogames/videogames.component.ts
@@ -6,35 +6,51 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-videogames',
   templateUrl: './videogames.component.html',
-  styleUrls: ['./videogames.component.scss']
+  styleUrls: ['./videogames.component.scss'],
 })
 export class VideogamesComponent implements OnInit {
   public videogamesList: VideogamesInterface[] = [];
-  public inputSearch: string ='';
-  
-  constructor(private videogamesService: VideogamesService, private router: Router) { }
+  public inputSearch: string = '';
+
+  constructor(
+    private videogamesService: VideogamesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getVideogames();
   }
 
-  public getVideogames =() => {
-    this.videogamesService.getVideogames().subscribe((data:any) =>{
+  public getVideogames = () => {
+    this.videogamesService.getVideogames().subscribe((data: any) => {
       this.videogamesList = data;
-    })
-  }
+    });
+  };
 
-  public filterVideogames =(filter: string) => {
-    if(filter === ''){
+  public filterVideogames = (filter: string) => {
+    if (filter === '') {
       this.getVideogames();
     } else {
-      this.videogamesList = this.videogamesList.filter(game => game.title.toLowerCase().includes(filter.toLowerCase()));
+      this.videogamesList = this.videogamesList.filter((game) =>
+        game.title.toLowerCase().includes(filter.toLowerCase())
+      );
     }
-  }
+  };
 
-  public selectedToEdit = (videogame:VideogamesInterface) => {
+  public selectedToEdit = (videogame: VideogamesInterface) => {
     this.videogamesService.sendItemToEdit(videogame);
     this.router.navigate(['gestion']);
-  }
+  };
 
+  public orderVideogamesByTitle = ( sort: string) => {
+    if (sort === 'mayor') {
+      this.videogamesList = this.videogamesList.sort((a, b) =>
+        a.title.localeCompare(b.title)
+      );
+    } else if (sort === 'menor') {
+      this.videogamesList = this.videogamesList.sort((a, b) =>
+        b.title.localeCompare(a.title)
+      );
+    }
+  };
 }
